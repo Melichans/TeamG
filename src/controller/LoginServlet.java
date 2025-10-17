8068a7ebaf988f660ab16833b6c0265ffcee1d28
k_yagi <k_yagi@LAPTOP-A26D0H8V>
2025-10-17 16:31:44 +0900

    testt

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import test.DBConnection;
-
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
--- src/controller/LogoutServlet.java
+++ src/controller/LogoutServlet.java
@@ -15,6 +15,6 @@ public class LogoutServlet extends HttpServlet {
         if (session != null) {
             session.invalidate();
         }
-        response.sendRedirect("controller/login.jsp");
+        response.sendRedirect("views/login.jsp");
     }
 }
